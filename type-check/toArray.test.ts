3d1d5eb8782cceac2acab5d027310653dcaac564
hyunwoo.jo <hwj@marpple.com>
2021-11-17 20:52:38 +0900

    test: add toArray

---
+++ type-check/toArray.test.ts
@@ -0,0 +1,30 @@
+import * as Test from "../src/types/Test";
+import { pipe, toArray, toAsync } from "../src";
+
+const { checks, check } = Test;
+
+function* range(n: number) {
+  let i = -1;
+  while (++i < n) {
+    yield i;
+  }
+}
+
+const res1 = toArray(range(3));
+const res2 = toArray("abc");
+
+const res1Promise = toArray(toAsync(range(3)));
+const res2Promise = toArray(toAsync("abc"));
+
+const res3 = pipe(range(3), toArray);
+const res3Promise = pipe(range(3), toAsync, toArray);
+
+checks([
+  check<typeof res1, number[], Test.Pass>(),
+  check<typeof res2, string[], Test.Pass>(),
+  check<typeof res1Promise, Promise<number[]>, Test.Pass>(),
+  check<typeof res2Promise, Promise<string[]>, Test.Pass>(),
+  check<typeof res2Promise, Promise<string[]>, Test.Pass>(),
+  check<typeof res3, number[], Test.Pass>(),
+  check<typeof res3Promise, Promise<number[]>, Test.Pass>(),
+]);
