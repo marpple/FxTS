f238e689bdd0765c10513acebda965e7df2c0720
hyunwoo.jo <hwj@marpple.com>
2021-11-17 20:52:19 +0900

    test: add reduce async

--- test/reduce.spec.ts
+++ test/reduce.spec.ts
@@ -1,6 +1,7 @@
-import { range, reduce } from "../src/index";
+import { pipe, range, reduce, toAsync } from "../src/index";
 
 const addNumber = (a: number, b: number) => a + b;
+const addNumberAsync = async (a: number, b: number) => a + b;
 
 describe("reduce", () => {
   describe("sync", () => {
@@ -18,6 +19,70 @@ describe("reduce", () => {
 
     it("should use the first value as the initial value if initial value is absent", () => {
       expect(reduce(addNumber, range(1, 6))).toEqual(15);
+    });
+  });
+
+  describe("async", () => {
+    it("should reduce `iterable` by the callback", async () => {
+      expect(await reduce(addNumber, 10, toAsync(range(1, 6)))).toEqual(25);
+    });
+
+    it("should use the first value as the initial value if initial value is absent", async () => {
+      expect(await reduce(addNumber, toAsync(range(1, 6)))).toEqual(15);
+    });
+
+    it("should reduce `AsyncIterable` by the callback with initial value", async () => {
+      expect(await reduce(addNumberAsync, 10, toAsync(range(1, 6)))).toEqual(
+        25,
+      );
+    });
+
+    it("should reduce 'AsyncIterable' by the callback", async () => {
+      expect(await reduce(addNumberAsync, toAsync(range(1, 6)))).toEqual(15);
+    });
+
+    it("should return rejected 'Promise' if an error occurs in the callback", async () => {
+      await expect(
+        reduce(
+          () => {
+            throw new Error("err");
+          },
+          0,
+          toAsync(range(1, 6)),
+        ),
+      ).rejects.toThrow("err");
+
+      await expect(
+        reduce(() => {
+          throw new Error("err");
+        }, toAsync(range(1, 6))),
+      ).rejects.toThrow("err");
+
+      await expect(
+        pipe(
+          toAsync(range(1, 6)),
+          reduce(() => {
+            throw new Error("err");
+          }),
+        ),
+      ).rejects.toThrow("err");
+    });
+
+    it("should return rejected 'Promise' if an error occurs in the callback", async () => {
+      await expect(
+        reduce(() => Promise.reject(new Error("err")), 0, toAsync(range(1, 6))),
+      ).rejects.toThrow("err");
+
+      await expect(
+        reduce(() => Promise.reject(new Error("err")), toAsync(range(1, 6))),
+      ).rejects.toThrow("err");
+
+      await expect(
+        pipe(
+          toAsync(range(1, 6)),
+          reduce(() => Promise.reject(new Error("err"))),
+        ),
+      ).rejects.toThrow("err");
     });
   });
 });
--- type-check/reduce.test.ts
+++ type-check/reduce.test.ts
@@ -1,5 +1,5 @@
 import * as Test from "../src/types/Test";
-import { reduce } from "../src";
+import { pipe, reduce, toAsync } from "../src";
 
 const { checks, check } = Test;
 
@@ -13,6 +13,43 @@ const res6 = reduce((a: string, b) => String(Number(a) + Number(b)), [1, 2, 3]);
 // const res7 = reduce((a, b) => a + b, Promise.resolve(0), [1, 2, 3]); // error
 // const res8 = reduce((a, b) => Promise.resolve(a + b), 0, [1, 2, 3]); // error
 
+const res9 = reduce((a, b) => a + b, 0, toAsync([1, 2, 3]));
+const res10 = reduce((a, b) => a + b, Promise.resolve(0), toAsync([1, 2, 3]));
+
+const res11 = pipe(
+  [1, 2, 3],
+  reduce((a, b) => a + b),
+);
+
+const res12 = pipe(
+  [1, 2, 3],
+  reduce((a: string, b) => String(Number(a) + Number(b))),
+);
+
+const res13 = pipe(
+  [1, 2, 3],
+  toAsync,
+  reduce((a, b) => a + b),
+);
+
+const res14 = pipe(
+  [1, 2, 3],
+  toAsync,
+  reduce((a, b) => Promise.resolve(a + b)),
+);
+
+const res15 = pipe(
+  [1, 2, 3],
+  toAsync,
+  reduce((a: string, b) => String(Number(a) + Number(b))),
+);
+
+const res16 = pipe(
+  [1, 2, 3],
+  toAsync,
+  reduce((a: string, b) => Promise.resolve(String(Number(a) + Number(b)))),
+);
+
 checks([
   check<typeof res1, undefined, Test.Pass>(),
   check<typeof res2, "seed", Test.Pass>(),
@@ -20,4 +57,17 @@ checks([
   check<typeof res4, number, Test.Pass>(),
   check<typeof res5, string, Test.Pass>(),
   check<typeof res6, string, Test.Pass>(),
+
+  // check<typeof res7, Promise<number>, Test.Pass>(),
+  // check<typeof res8, Promise<number>, Test.Pass>(),
+
+  check<typeof res9, Promise<number>, Test.Pass>(),
+  check<typeof res10, Promise<number>, Test.Pass>(),
+
+  check<typeof res11, number, Test.Pass>(),
+  check<typeof res12, string, Test.Pass>(),
+  check<typeof res13, Promise<number>, Test.Pass>(),
+  check<typeof res14, Promise<number>, Test.Pass>(),
+  check<typeof res15, Promise<string>, Test.Pass>(),
+  check<typeof res16, Promise<string>, Test.Pass>(),
 ]);
