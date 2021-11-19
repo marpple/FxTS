01355748f42f3713e5a3e823d1e6fffe4b51d0ef
Donghee Lee <stringdong@gmail.com>
2021-11-20 02:18:20 +0900

    test: add missing tests and fix format

--- test/isArray.spec.ts
+++ test/isArray.spec.ts
@@ -1,14 +1,15 @@
 import { isArray } from "../src/index";
 
-describe("isString", function() {
-  it.each(
-    [2, true, "a"]
-  )("given non array then should return false", function(s) {
-    const result = isArray(s);
-    expect(result).toEqual(false);
-  });
+describe("isString", function () {
+  it.each([2, true, "a"])(
+    "given non array then should return false",
+    function (s) {
+      const result = isArray(s);
+      expect(result).toEqual(false);
+    },
+  );
 
-  it("given array then should return true", function() {
+  it("given array then should return true", function () {
     const result = isArray([1, 2, 3]);
     expect(result).toEqual(true);
   });
