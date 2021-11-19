a661eb47e6eb383348197ba20529ac2c5a08a5d0
Donghee Lee <stringdong@gmail.com>
2021-11-19 23:06:25 +0900

    test: add unit tests

---
+++ test/isString.spec.ts
@@ -0,0 +1,15 @@
+import { isString } from "../src/index";
+
+describe("isString", function() {
+  it.each(
+    [2, true]
+  )("given non string then should return false", function(s) {
+    const result = isString(s);
+    expect(result).toEqual(false);
+  });
+
+  it("given string then should return true", function() {
+    const result = isString("a");
+    expect(result).toEqual(true);
+  });
+});
