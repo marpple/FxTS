477c0da2b502284e1dbbed751533fef8666e2a90
Donghee Lee <stringdong@gmail.com>
2021-11-20 11:58:21 +0900

    test: check all of the js types in the unit tests

--- test/isString.spec.ts
+++ test/isString.spec.ts
@@ -1,10 +1,13 @@
 import { isString } from "../src/index";
 
 describe("isString", function () {
-  it.each([2, true])("given non string then should return false", function (s) {
-    const result = isString(s);
-    expect(result).toEqual(false);
-  });
+  it.each([undefined, null, true, 1, Symbol("a"), () => null])(
+    "given non string then should return false",
+    function (s) {
+      const result = isString(s);
+      expect(result).toEqual(false);
+    },
+  );
 
   it("given string then should return true", function () {
     const result = isString("a");
