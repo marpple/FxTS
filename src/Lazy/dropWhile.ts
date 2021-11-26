030d5eca61dac371288e07da28e01f201f0f5b3a
hyunwoo.jo <hwj@marpple.com>
2021-11-26 15:41:10 +0900

    feat: dropWhile async

--- src/Lazy/dropWhile.ts
+++ src/Lazy/dropWhile.ts
@@ -1,6 +1,7 @@
 import IterableInfer from "../types/IterableInfer";
+import ReturnIterableIteratorType from "../types/ReturnIterableIteratorType";
 import { AsyncFunctionException } from "../_internal/error";
-import { isIterable } from "../_internal/utils";
+import { isAsyncIterable, isIterable } from "../_internal/utils";
 
 function* sync<A, B>(f: (a: A) => B, iterable: Iterable<A>) {
   for (const a of iterable) {
@@ -16,29 +17,52 @@ function* sync<A, B>(f: (a: A) => B, iterable: Iterable<A>) {
   }
 }
 
+async function* async<A, B>(
+  f: (a: A) => B,
+  iterable: AsyncIterable<A>,
+): AsyncIterableIterator<A> {
+  for await (const a of iterable) {
+    if (await f(a)) {
+      continue;
+    }
+
+    yield a;
+  }
+}
+
 function dropWhile<A, B = unknown>(
   f: (a: A) => B,
   iterable: Iterable<A>,
 ): IterableIterator<A>;
 
-function dropWhile<A extends Iterable<unknown>, B>(
+function dropWhile<A, B = unknown>(
+  f: (a: A) => B,
+  iterable: AsyncIterable<A>,
+): AsyncIterableIterator<A>;
+
+function dropWhile<A extends Iterable<unknown> | AsyncIterable<unknown>, B>(
   f: (a: IterableInfer<A>) => B,
-): (iterable: A) => IterableIterator<IterableInfer<A>>;
+): (iterable: A) => ReturnIterableIteratorType<A>;
 
 function dropWhile<A extends Iterable<unknown>, B>(
   f: (a: IterableInfer<A>) => B,
   iterable?: A,
 ):
   | IterableIterator<IterableInfer<A>>
-  | ((iterable: A) => IterableIterator<IterableInfer<A>>) {
+  | AsyncIterableIterator<IterableInfer<A>>
+  | ((iterable: A) => ReturnIterableIteratorType<A>) {
   if (iterable === undefined) {
     return (iterable: A) => {
-      return dropWhile(f, iterable as Iterable<IterableInfer<A>>);
+      return dropWhile(f, iterable as any) as ReturnIterableIteratorType<A>;
     };
   }
 
   if (isIterable(iterable)) {
     return sync(f, iterable as Iterable<IterableInfer<A>>);
+  }
+
+  if (isAsyncIterable(iterable)) {
+    return async(f, iterable as AsyncIterable<IterableInfer<A>>);
   }
 
   throw new TypeError("iterable must be type of Iterable or AsyncIterable");
--- test/Lazy/dropWhile.spec.ts
+++ test/Lazy/dropWhile.spec.ts
@@ -1,4 +1,11 @@
-import { dropWhile, filter, map, pipe, toArray } from "../../src/index";
+import {
+  dropWhile,
+  filter,
+  map,
+  pipe,
+  toArray,
+  toAsync,
+} from "../../src/index";
 import { AsyncFunctionException } from "../../src/_internal/error";
 
 describe("drop", function () {
@@ -26,6 +33,43 @@ describe("drop", function () {
     it("should throw an error when the callback is asynchronous", function () {
       const res = () => [...dropWhile(async (a) => a > 5, [1, 2, 3, 4, 5])];
       expect(res).toThrowError(new AsyncFunctionException());
+    });
+  });
+
+  describe("async", function () {
+    it("should be dropped elements until the value applied to callback returns falsey", async function () {
+      const acc = [];
+      for await (const a of dropWhile((a) => a < 3, toAsync([1, 2, 3, 4, 5]))) {
+        acc.push(a);
+      }
+      expect(acc).toEqual([3, 4, 5]);
+    });
+
+    it("should be able to be used as a curried function in the pipeline", async function () {
+      const res = await pipe(
+        toAsync([1, 2, 3, 4, 5, 6, 7, 8]),
+        map((a) => a + 10),
+        filter((a) => a % 2 === 0),
+        dropWhile((a) => a < 16),
+        toArray,
+      );
+
+      expect(res).toEqual([16, 18]);
+    });
+
+    it("should be able to handle an error when asynchronous", async function () {
+      await expect(
+        pipe(
+          toAsync([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+          dropWhile((a) => {
+            if (a > 5) {
+              throw new Error("err");
+            }
+            return true;
+          }),
+          toArray,
+        ),
+      ).rejects.toThrow("err");
     });
   });
 });
