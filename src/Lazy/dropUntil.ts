8c31d446f51ab107a7dc9f84aa0864829e7847a8
hyunwoo.jo <hwj@marpple.com>
2021-11-29 10:52:28 +0900

    feat: dropUntil async

--- src/Lazy/dropUntil.ts
+++ src/Lazy/dropUntil.ts
@@ -1,3 +1,5 @@
+import { isAsyncIterable, isIterable } from "../_internal/utils";
+
 function* sync<A, B>(f: (a: A) => B, iterable: Iterable<A>) {
   const iterator = iterable[Symbol.iterator]();
   const iterableIterator = {
@@ -17,13 +19,48 @@ function* sync<A, B>(f: (a: A) => B, iterable: Iterable<A>) {
   yield* iterableIterator;
 }
 
+async function* async<A, B>(f: (a: A) => B, iterable: AsyncIterable<A>) {
+  const iterator = iterable[Symbol.asyncIterator]();
+  const iterableIterator = {
+    [Symbol.asyncIterator]() {
+      return iterator;
+    },
+  };
+
+  for await (const a of iterableIterator) {
+    if (!(await f(a))) {
+      continue;
+    } else {
+      break;
+    }
+  }
+
+  yield* iterableIterator;
+}
+
 function dropUntil<A, B = unknown>(
   f: (a: A) => B,
   iterable: Iterable<A>,
 ): IterableIterator<A>;
 
-function dropUntil<A, B>(f: (a: A) => B, iterable: Iterable<A>) {
-  return sync(f, iterable);
+function dropUntil<A, B = unknown>(
+  f: (a: A) => B,
+  iterable: AsyncIterable<A>,
+): AsyncIterableIterator<A>;
+
+function dropUntil<A, B>(
+  f: (a: A) => B,
+  iterable: Iterable<A> | AsyncIterable<A>,
+): IterableIterator<A> | AsyncIterableIterator<A> {
+  if (isIterable(iterable)) {
+    return sync(f, iterable);
+  }
+
+  if (isAsyncIterable(iterable)) {
+    return async(f, iterable);
+  }
+
+  throw new TypeError("iterable must be type of Iterable or AsyncIterable");
 }
 
 export default dropUntil;
--- test/Lazy/dropUntil.spec.ts
+++ test/Lazy/dropUntil.spec.ts
@@ -1,4 +1,4 @@
-import { dropUntil } from "../../src";
+import { dropUntil, toAsync } from "../../src";
 
 describe("dropUntil", function () {
   describe("sync", function () {
@@ -6,6 +6,26 @@ describe("dropUntil", function () {
       const arr = [1, 2, 3, 4, 5, 1, 2];
       const acc = [];
       for (const a of dropUntil((a) => a > 3, arr)) {
+        acc.push(a);
+      }
+      expect(acc).toEqual([5, 1, 2]);
+    });
+  });
+
+  describe("async", function () {
+    it("should be dropped elements until the value applied to callback returns truly", async function () {
+      const arr = [1, 2, 3, 4, 5, 1, 2];
+      const acc = [];
+      for await (const a of dropUntil((a) => a > 3, toAsync(arr))) {
+        acc.push(a);
+      }
+      expect(acc).toEqual([5, 1, 2]);
+    });
+
+    it("should be dropped elements until the value applied to async callback returns truly", async function () {
+      const arr = [1, 2, 3, 4, 5, 1, 2];
+      const acc = [];
+      for await (const a of dropUntil(async (a) => a > 3, toAsync(arr))) {
         acc.push(a);
       }
       expect(acc).toEqual([5, 1, 2]);
