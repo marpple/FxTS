6b8292e01d7454ab629618c14e4adcdf2238a518
hyunwoo.jo <hwj@marpple.com>
2021-11-29 11:42:47 +0900

    feat: dropUntil concurrent

--- src/Lazy/dropUntil.ts
+++ src/Lazy/dropUntil.ts
@@ -2,6 +2,7 @@ import IterableInfer from "../types/IterableInfer";
 import ReturnIterableIteratorType from "../types/ReturnIterableIteratorType";
 import { AsyncFunctionException } from "../_internal/error";
 import { isAsyncIterable, isIterable } from "../_internal/utils";
+import concurrent, { isConcurrent } from "./concurrent";
 
 function* sync<A, B>(f: (a: A) => B, iterable: Iterable<A>) {
   const iterator = iterable[Symbol.iterator]();
@@ -23,7 +24,10 @@ function* sync<A, B>(f: (a: A) => B, iterable: Iterable<A>) {
   }
 }
 
-async function* async<A, B>(f: (a: A) => B, iterable: AsyncIterable<A>) {
+async function* asyncSequential<A, B>(
+  f: (a: A) => B,
+  iterable: AsyncIterable<A>,
+) {
   const iterator = iterable[Symbol.asyncIterator]();
   const iterableIterator = {
     [Symbol.asyncIterator]() {
@@ -38,6 +42,73 @@ async function* async<A, B>(f: (a: A) => B, iterable: AsyncIterable<A>) {
   }
 }
 
+function async<A, B>(
+  f: (a: A) => B,
+  iterable: AsyncIterable<A>,
+): AsyncIterableIterator<A> {
+  let iterator: AsyncIterator<A>;
+  return {
+    [Symbol.asyncIterator]() {
+      return this;
+    },
+
+    async next(_concurrent: any) {
+      if (iterator === undefined) {
+        iterator = isConcurrent(_concurrent)
+          ? asyncSequential(f, concurrent(_concurrent.length, iterable))
+          : asyncSequential(f, iterable);
+      }
+
+      return iterator.next(_concurrent);
+    },
+  };
+}
+
+/**
+ * Returns Iterable/AsyncIterable excluding elements dropped from the beginning.
+ * Elements are deleted until the value applied to f returns truly. (It is deleted including the first value applied as true)
+ *
+ * @example
+ * ```ts
+ * const iter = dropUntil((a) => a < 3, [1, 2, 3, 4, 5, 1, 2]);
+ * iter.next(); // {done:false, value: 3}
+ * iter.next(); // {done:false, value: 4}
+ * iter.next(); // {done:false, value: 5}
+ *
+ * // with pipe
+ * pipe(
+ *  [1, 2, 3, 4, 5, 1, 2],
+ *  dropUntil((a) => a > 3),
+ *  toArray,
+ * ); // [5, 1, 2]
+ *
+ * await pipe(
+ *  Promise.resolve([1, 2, 3, 4, 5, 1, 2]),
+ *  dropUntil((a) => a > 3),
+ *  toArray,
+ * ); // [5, 1, 2]
+ *
+ * // if you want to use asynchronous callback
+ * await pipe(
+ *  Promise.resolve([1, 2, 3, 4, 5, 1, 2]),
+ *  toAsync,
+ *  dropUntil(async (a) => a > 3),
+ *  toArray,
+ * ); // [5, 1, 2]
+ *
+ * // with toAsync
+ * await pipe(
+ *  [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3), Promise.resolve(4),
+ * Promise.resolve(5), Promise.resolve(1), Promise.resolve(2)],
+ *  toAsync,
+ *  dropUntil((a) => a > 3),
+ *  toArray,
+ * ); // [5, 1, 2]
+ *
+ * see {@link https://fxts.dev/docs/pipe | pipe}, {@link https://fxts.dev/docs/toAsync | toAsync},
+ * {@link https://fxts.dev/docs/toArray | toArray}
+ * ```
+ */
 function dropUntil<A, B = unknown>(
   f: (a: A) => B,
   iterable: Iterable<A>,
