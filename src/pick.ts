66715d946b4311e44aaf41cd1d32663e98aaf75c
Dohyeong <panamera1594@gmail.com>
2021-12-01 18:09:22 +0900

    refactor: improve performance of 'pick' and 'omit'

--- src/pick.ts
+++ src/pick.ts
@@ -2,24 +2,26 @@ import IterableInfer from "./types/IterableInfer";
 import { isAsyncIterable, isIterable } from "./_internal/utils";
 import toArray from "./toArray";
 
+function inner<T>(obj: T, keys: Set<keyof T>) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([k]) => keys.has(k as keyof T)),
+  );
+}
+
 function sync<T extends object, U extends Iterable<keyof T>>(
   iterable: U,
   obj: T,
 ): Pick<T, IterableInfer<U>> {
-  const keys = Array.isArray(iterable) ? iterable : Array.from(iterable);
-  return Object.fromEntries(
-    Object.entries(obj).filter(([k]) => keys.includes(k as keyof T)),
-  ) as Pick<T, IterableInfer<U>>;
+  const keys = new Set(iterable);
+  return inner(obj, keys) as Pick<T, IterableInfer<U>>;
 }
 
 async function async<T extends object, U extends AsyncIterable<keyof T>>(
   iterable: U,
   obj: T,
 ): Promise<Pick<T, IterableInfer<U>>> {
-  const keys = await toArray(iterable);
-  return Object.fromEntries(
-    Object.entries(obj).filter(([k]) => keys.includes(k as keyof T)),
-  ) as any;
+  const keys = new Set(await toArray(iterable));
+  return inner(obj, keys) as Pick<T, IterableInfer<U>>;
 }
 
 function pick<T extends object, U extends readonly []>(
