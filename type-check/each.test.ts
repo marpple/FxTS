c3953aa7a6bf64f9c3f6903bb94e05068062d1af
hyunwoo.jo <hwj@marpple.com>
2021-12-03 13:46:41 +0900

    test: add each test case

--- type-check/each.test.ts
+++ type-check/each.test.ts
@@ -1,14 +1,16 @@
 import * as Test from "../src/types/Test";
-import { each, pipe, toAsync } from "../src";
+import { each, pipe, take, toAsync } from "../src";
 
 const { checks, check } = Test;
 
 const res1 = each(console.log, [1, 2, 3]);
 const res2 = pipe([1, 2, 3], each(console.log));
-const res3 = pipe([1, 2, 3], toAsync, each(console.log));
+const res3 = pipe([1, 2, 3], take(1), each(console.log));
+const res4 = pipe([1, 2, 3], toAsync, each(console.log));
 
 checks([
   check<typeof res1, void, Test.Pass>(),
   check<typeof res2, void, Test.Pass>(),
-  check<typeof res3, Promise<void>, Test.Pass>(),
+  check<typeof res3, void, Test.Pass>(),
+  check<typeof res4, Promise<void>, Test.Pass>(),
 ]);
