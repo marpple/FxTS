e9ddb00a2e9e7864a0d1464909857957243cba84
Jake Son <sonjeabin@gmail.com>
2021-12-04 12:00:02 +0900

    test: add type test of 'Prepend'

---
+++ type-check/types/Prepend.test.ts
@@ -0,0 +1,9 @@
+import * as Test from "../../src/types/Test";
+import Prepend from "../../src/types/Prepend";
+
+const { checks, check } = Test;
+
+checks([
+  check<Prepend<[], number>, [number], Test.Pass>(),
+  check<Prepend<[1, true], string>, [string, 1, true], Test.Pass>(),
+]);
