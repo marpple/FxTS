7ed93eb44c6745fdcd2ae7962df77bd01b8ee312
Jake Son <sonjeabin@gmail.com>
2021-12-04 15:19:01 +0900

    feat: update type test of 'curry'

--- type-check/curry.test.ts
+++ type-check/curry.test.ts
@@ -1,5 +1,7 @@
 import * as Test from "../src/types/Test";
 import Curry from "../src/types/Curry";
+import { curry, pipe, toAsync } from "../src";
+import { filter } from "../src/Lazy";
 
 const { checks, check } = Test;
 
@@ -11,6 +13,11 @@ const res1 = curried("a");
 const res2 = res1();
 const res3 = res2(true);
 const res4 = res2(false, 1, 2, 3);
+
+const strictEq = curry(<T>(left: T, right: T) => left === right);
+
+const res5 = pipe([1, 1, 1, 2, 2, 2], filter(strictEq(2)));
+const res6 = pipe([1, 1, 1, 2, 2, 2], toAsync, filter(strictEq(2)));
 
 checks([
   check<
@@ -25,4 +32,6 @@ checks([
   >(),
   check<typeof res3, string, Test.Pass>(),
   check<typeof res4, string, Test.Pass>(),
+  check<typeof res5, IterableIterator<number>, Test.Pass>(),
+  check<typeof res6, AsyncIterableIterator<number>, Test.Pass>(),
 ]);
