a814e2e56170c26223520008cc58dd6b1a113516
hyunwoo.jo <hwj@marpple.com>
2021-12-08 15:54:36 +0900

    test: nested concurrent

--- test/Lazy/concurrent.spec.ts
+++ test/Lazy/concurrent.spec.ts
@@ -1,4 +1,13 @@
-import { concurrent, delay, map, pipe, range, toAsync } from "../../src/index";
+import {
+  concurrent,
+  delay,
+  filter,
+  map,
+  peek,
+  pipe,
+  range,
+  toAsync,
+} from "../../src/index";
 import { callFuncAfterTime } from "../utils";
 
 describe("concurrent", function () {
@@ -50,6 +59,47 @@ describe("concurrent", function () {
     expect(fn).toBeCalled();
     expect(arr).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   }, 550);
+
+  it("should be affected only one concurrent below it, when nested concurrent", async function () {
+    let fn: jest.Mock<any, any>;
+    fn = jest.fn();
+    let concurrent10Count = 0;
+    let concurrent2Count = 0;
+
+    const iter = pipe(
+      toAsync(range(Infinity)),
+      peek(() => concurrent10Count++),
+      map((a) => delay(100, a)),
+      concurrent(10),
+      peek(() => concurrent2Count++),
+      map((a) => delay(100, a)),
+      filter((a) => a % 2 === 0),
+      concurrent(2),
+    );
+
+    callFuncAfterTime(fn, 300);
+    await iter.next();
+    await iter.next();
+    expect(fn).toBeCalled();
+    expect(concurrent2Count).toEqual(4);
+    expect(concurrent10Count).toEqual(10);
+
+    fn = jest.fn();
+    callFuncAfterTime(fn, 200);
+    await iter.next();
+    await iter.next();
+    expect(fn).toBeCalled();
+    expect(concurrent2Count).toEqual(8);
+    expect(concurrent10Count).toEqual(10);
+
+    fn = jest.fn();
+    callFuncAfterTime(fn, 300);
+    await iter.next();
+    await iter.next();
+    expect(fn).toBeCalled();
+    expect(concurrent2Count).toEqual(12);
+    expect(concurrent10Count).toEqual(20);
+  }, 850);
 
   it("should return IteratorReturnResult after all consuming 'AsyncIterable'", async function () {
     const fn = jest.fn();
