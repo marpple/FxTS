823e2959ec7c9f40b9477e3ff945c90569a68fb0
hugo kim <k7120792@gmail.com>
2021-12-16 03:14:51 +0900

    fix: change wrong pattern for test

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,8 @@
+const path = require('path');
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: [path.join(__dirname, "test/**/*.spec.ts")],
 };
