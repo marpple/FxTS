4a9020c7733d2bf8429695043bc5a547f5b39b9b
Dohyeong <panamera1594@gmail.com>
2021-12-24 01:23:12 +0900

    test: add test about type of the `evolve` function

--- test/evolve.spec.ts
+++ test/evolve.spec.ts
@@ -15,6 +15,15 @@ describe("evolve", function () {
     expect(res).toEqual({ a: "2", b: 3, c: "4" });
   });
 
+  it("should apply the `identity` function to a property if there is no matched transform function", function () {
+    const obj = { a: 1, b: 2, c: 3 };
+    const transformation = {
+      b: add1,
+    };
+    const res = evolve(transformation, obj);
+    expect(res).toEqual({ a: 1, b: 3, c: 3 });
+  });
+
   it("should be able to be used as a curried function in the pipeline", function () {
     const obj = { a: 1, b: 2, c: 3 };
     const transformation = {
@@ -24,5 +33,23 @@ describe("evolve", function () {
     };
     const res = pipe(obj, evolve(transformation), Object.values);
     expect(res).toEqual(["2", 3, "4"]);
+  });
+
+  it("should be handled nested objects", function () {
+    const add1String = (a: number) => String(a + 1);
+    const obj = { a: 1, b: 2, c: { d: 3, e: 4 }, f: true };
+    const transformation = {
+      a: add1String,
+      b: add1String,
+      c: (obj: { d: number; e: number }) =>
+        evolve({ d: add1String, e: add1String }, obj),
+    };
+    const res = evolve(transformation, obj);
+    expect(res).toEqual({
+      a: "2",
+      b: "3",
+      c: { d: "4", e: "5" },
+      f: true,
+    });
   });
 });
---
+++ type-check/evolve.test.ts
@@ -0,0 +1,55 @@
+import * as Test from "../src/types/Test";
+import { evolve, pipe } from "../src";
+
+const { checks, check } = Test;
+
+const obj = {
+  a: 0,
+  b: "1",
+  c: true,
+};
+
+const transform1 = {
+  a: Boolean,
+  b: Number,
+  c: String,
+};
+
+const transform2 = {
+  a: Boolean,
+  c: String,
+};
+
+const res1 = evolve(transform1, obj);
+const res2 = evolve(transform2, obj);
+
+// with pipe
+const res3 = pipe(obj, evolve(transform1));
+const res4 = pipe(obj, evolve(transform2));
+
+// nested object
+const obj2 = {
+  a: 0,
+  b: { c: 1, d: 2 },
+  e: 3,
+};
+
+const transform3 = {
+  a: (v: number) => v + 1,
+  b: (obj: typeof obj2["b"]) => evolve({ c: Boolean, d: () => null }, obj),
+  e: String,
+};
+
+const res5 = evolve(transform3, obj2);
+
+checks([
+  check<typeof res1, { a: boolean; b: number; c: string }, Test.Pass>(),
+  check<typeof res2, { a: boolean; b: string; c: string }, Test.Pass>(),
+  check<typeof res3, { a: boolean; b: number; c: string }, Test.Pass>(),
+  check<typeof res4, { a: boolean; b: string; c: string }, Test.Pass>(),
+  check<
+    typeof res5,
+    { a: number; b: { c: boolean; d: null }; e: string },
+    Test.Pass
+  >(),
+]);
