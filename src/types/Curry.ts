552d695c5534df164196e46cdb5255eb2951b2af
degurii <sansam709@gmail.com>
2022-02-08 00:07:54 +0900

    fix: fix return type in Curry type
    When a curried function is given as a callback of a map or pipe,
    the type is incorrectly inferred if the curried function is executed.
    
    More precisely, it infers incorrectly not only in pipe and map,
    but also in function of the type compatible with <R>(f:()=>R)=>R.

--- src/types/Curry.ts
+++ src/types/Curry.ts
@@ -11,9 +11,10 @@ type Gaps<L extends any[]> = Cast<CleanGaps<{ [K in keyof L]?: L[K] }>, any[]>;
 type Curry<F extends (...args: any[]) => any> = <
   T extends any[],
   G = Drop<Length<T>, Parameters<F>>,
-  R = ReturnType<F>,
 >(
   ...args: Cast<T, Gaps<Parameters<F>>>
-) => G extends [any, ...any[]] ? Curry<(...args: G) => R> : R;
+) => G extends [any, ...any[]]
+  ? Curry<(...args: G) => ReturnType<F>>
+  : ReturnType<F>;
 
 export default Curry;
--- test/curry.spec.ts
+++ test/curry.spec.ts
@@ -1,4 +1,4 @@
-import { curry } from "../src";
+import { curry, filter, map, pipe, toArray } from "../src";
 
 describe("curry", function () {
   it("should not change a unary function", function () {
@@ -16,5 +16,22 @@ describe("curry", function () {
     expect(curried(1, 2, 3)).toBe(6);
     expect(curried()(1, 2, 3)).toBe(6);
     expect(curried(1)()(2, 3)).toBe(6);
+  });
+
+  it("curried function should be able to be used within the pipeline.", function () {
+    const add = curry((a: number, b: number, c: number) => a + b + c);
+    const multiply = curry((a: number, b: number) => a * b);
+    const lt = curry((l: number, r: number) => l < r);
+
+    expect(pipe(1, add(2, 3), multiply(10))).toBe(60);
+    expect(
+      pipe(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        map(add(2, 3)),
+        map(multiply(10)),
+        filter(lt(100)),
+        toArray,
+      ),
+    ).toEqual([110, 120, 130, 140, 150]);
   });
 });
