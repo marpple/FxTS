ed632f812a2b8a00cfbb24b21094222347a8de3a
Yunseok Jeong <sansam709@gmail.com>
2022-02-11 11:31:41 +0900

    fix: fix return type in Curry type (#140)
    * fix: fix return type in Curry type
    
    When a curried function is given as a callback of a map or pipe,
    the type is incorrectly inferred if the curried function is executed.
    
    More precisely, it infers incorrectly not only in pipe and map,
    but also in function of the type compatible with <R>(f:()=>R)=>R.
    
    * test: add type-check test case in curry
    
    * Update type-check/curry.test.ts
    
    Co-authored-by: Hyunwoo.Jo <xodltus@naver.com>

--- type-check/curry.test.ts
+++ type-check/curry.test.ts
@@ -1,7 +1,7 @@
 import * as Test from "../src/types/Test";
 import Curry from "../src/types/Curry";
 import { curry, pipe, toAsync } from "../src";
-import { filter } from "../src/Lazy";
+import { filter, map } from "../src/Lazy";
 
 const { checks, check } = Test;
 
@@ -15,9 +15,13 @@ const res3 = res2(true);
 const res4 = res2(false, 1, 2, 3);
 
 const strictEq = curry(<T>(left: T, right: T) => left === right);
+const add = curry((a: number, b: number) => a + b);
 
 const res5 = pipe([1, 1, 1, 2, 2, 2], filter(strictEq(2)));
 const res6 = pipe([1, 1, 1, 2, 2, 2], toAsync, filter(strictEq(2)));
+const res7 = pipe(1, add(2), add(3));
+const res8 = pipe([1, 2, 3], map(add(10)), map(add(100)));
+const res9 = pipe([1, 2, 3], toAsync, map(add(10)), map(add(100)));
 
 checks([
   check<
@@ -34,4 +38,7 @@ checks([
   check<typeof res4, string, Test.Pass>(),
   check<typeof res5, IterableIterator<number>, Test.Pass>(),
   check<typeof res6, AsyncIterableIterator<number>, Test.Pass>(),
+  check<typeof res7, number, Test.Pass>(),
+  check<typeof res8, IterableIterator<number>, Test.Pass>(),
+  check<typeof res9, AsyncIterableIterator<number>, Test.Pass>(),
 ]);
