8834f2f17551d65b7313fc20e39cb49be28065ac
sunrabbit123 <qudwls185@naver.com>
2022-04-11 15:33:44 +0900

    feat: update return type remove Curry types

--- src/gte.ts
+++ src/gte.ts
@@ -1,6 +1,3 @@
-import curry from "./curry";
-import Curry from "./types/Curry";
-
 /**
  * Returns true if the first argument is greater or equal than the second; false otherwise.
  *
@@ -18,19 +15,23 @@ import Curry from "./types/Curry";
  * filter(gte("b"), ["a"]) // Itreable<[]>
  * ```
  */
-function gte(a: string): Curry<(b: string) => boolean>;
-function gte(a: number): Curry<(b: number) => boolean>;
+function gte(a: string | number): (b: string | number) => boolean;
 function gte(a: string, b: string): boolean;
 function gte(a: number, b: number): boolean;
 
 function gte(
   a: string | number,
   _b?: string | number,
-): Curry<(b: string | number) => boolean> | boolean {
+): ((b: string | number) => boolean) | boolean {
   if (_b === undefined) {
-    return curry((c: string | number, b: string | number): boolean => b >= c)(
-      a,
-    );
+    return (b: string | number): boolean => {
+      if (typeof a !== typeof b) {
+        throw new TypeError(
+          "The values you want to compare must be of the same type",
+        );
+      }
+      return b >= a;
+    };
   }
 
   return a >= _b;
