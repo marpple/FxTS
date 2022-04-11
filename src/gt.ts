a75f3381a7ec1bb6973c1475afa033c51ef53cbc
sunrabbit123 <qudwls185@naver.com>
2022-04-11 15:33:44 +0900

    feat: update return type remove Curry types

--- src/gt.ts
+++ src/gt.ts
@@ -1,6 +1,3 @@
-import curry from "./curry";
-import Curry from "./types/Curry";
-
 /**
  * Returns true if the first argument is greater than the second; false otherwise.
  *
@@ -17,17 +14,24 @@ import Curry from "./types/Curry";
  * filter(gt("b"), ["a", "b"]) // Itreable<[]>
  * ```
  */
-function gt(a: string): Curry<(b: string) => boolean>;
-function gt(a: number): Curry<(b: number) => boolean>;
+
+function gt(a: string | number): (b: string | number) => boolean;
 function gt(a: string, b: string): boolean;
 function gt(a: number, b: number): boolean;
 
 function gt(
   a: string | number,
   _b?: string | number,
-): Curry<(b: string | number) => boolean> | boolean {
+): ((b: string | number) => boolean) | boolean {
   if (_b === undefined) {
-    return curry((c: string | number, b: string | number): boolean => b > c)(a);
+    return (b: string | number): boolean => {
+      if (typeof a !== typeof b) {
+        throw new TypeError(
+          "The values you want to compare must be of the same type",
+        );
+      }
+      return b > a;
+    };
   }
 
   return a > _b;
--- src/lte.ts
+++ src/lte.ts
@@ -1,6 +1,3 @@
-import curry from "./curry";
-import Curry from "./types/Curry";
-
 /**
  * Returns true if the first argument is less or equal than the second; false otherwise.
  *
@@ -18,19 +15,23 @@ import Curry from "./types/Curry";
  * filter(lte("b"), ["c", "d"]) // Itreable<[]>
  * ```
  */
-function lte(a: string): Curry<(b: string) => boolean>;
-function lte(a: number): Curry<(b: number) => boolean>;
+function lte(a: string | number): (b: string | number) => boolean;
 function lte(a: string, b: string): boolean;
 function lte(a: number, b: number): boolean;
 
 function lte(
   a: string | number,
   _b?: string | number,
-): Curry<(b: string | number) => boolean> | boolean {
+): ((b: string | number) => boolean) | boolean {
   if (_b === undefined) {
-    return curry((c: string | number, b: string | number): boolean => b <= c)(
-      a,
-    );
+    return (b: string | number): boolean => {
+      if (typeof a !== typeof b) {
+        throw new TypeError(
+          "The values you want to compare must be of the same type",
+        );
+      }
+      return b <= a;
+    };
   }
 
   return a <= _b;
