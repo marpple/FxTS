2d7a6bd4c7c0852f9034fb52086dc912bbfad8da
Dohyeong <panamera1594@gmail.com>
2022-04-12 22:21:45 +0900

    chore: prettier fix

--- src/types/ReturnPipeType.ts
+++ src/types/ReturnPipeType.ts
@@ -5,7 +5,7 @@ import Prepend from "./Prepend";
 type ReturnPipeType<T extends any[]> = T extends [
   a: infer A,
   b: infer B,
-  ...args: any[]
+  ...args: any[],
 ]
   ? A extends Promise<any>
     ? ReturnPipeType<Prepend<Drop<2, T>, Promise<Awaited<B>>>>
