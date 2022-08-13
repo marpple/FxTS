316b9f43f20be1d96925611ed38d1644d277ddb4
hg-pyun <phg2491@gmail.com>
2022-08-13 23:53:19 +0900

    fix(sum): sum function logic - only supoort number type - empty array returns zero

--- src/sum.ts
+++ src/sum.ts
@@ -14,19 +14,33 @@ import { isAsyncIterable, isIterable } from "./_internal/utils";
  * await sum(toAsync(['a', 'b', 'c'])); // 'abc'
  * ```
  */
-function sum<
-  A extends
-    | Iterable<number>
-    | AsyncIterable<number>
-    | Iterable<string>
-    | AsyncIterable<string>,
->(iterable: A): ReturnValueType<A> {
+function sum<A extends readonly []>(iterable: A): 0;
+function sum<A extends Iterable<number> | AsyncIterable<number>>(
+  iterable: A,
+): ReturnValueType<A>;
+
+function sum<A extends Iterable<number> | AsyncIterable<number> | readonly []>(
+  iterable: A,
+): ReturnValueType<A> | 0 {
   if (isIterable(iterable)) {
+    const iterator = iterable[Symbol.iterator]();
+    const { done } = iterator.next();
+    if (done) {
+      return 0;
+    }
+
     return reduce(add, iterable) as ReturnValueType<A>;
   }
 
   if (isAsyncIterable(iterable)) {
-    return reduce(add, iterable) as ReturnValueType<A>;
+    const iterator = iterable[Symbol.asyncIterator]();
+    return iterator.next().then(({ done }) => {
+      if (done) {
+        return 0;
+      }
+
+      return reduce(add, iterable) as ReturnValueType<A>;
+    });
   }
 
   throw new TypeError("'iterable' must be type of Iterable or AsyncIterable");
