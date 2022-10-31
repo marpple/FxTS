c92676371023dcbf96a94f218c9010dddc7fcf07
hyunwoo.jo <hwj@marpple.com>
2022-10-31 15:47:54 +0900

    feat: assert to type definition on sum function

--- src/sum.ts
+++ src/sum.ts
@@ -17,11 +17,14 @@ function sum<A extends Iterable<number> | AsyncIterable<number>>(
   ? number
   : A extends AsyncIterable<number>
   ? Promise<number>
-  : never {
+  : never;
+function sum<A extends Iterable<number> | AsyncIterable<number>>(
+  iterable: A,
+): number | Promise<number> {
   if (Array.isArray(iterable)) {
     return iterable.reduce(add, 0);
   } else if (isIterable(iterable)) {
-    return reduce(add, 0, iterable);
+    return reduce<number, number>((a, b) => add(a, b), 0, iterable);
   } else if (isAsyncIterable(iterable)) {
     return reduce<number, number>(add, Promise.resolve(0), iterable);
   }
