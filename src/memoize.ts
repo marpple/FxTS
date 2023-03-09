bafe509876b608edca01319dcf193b05099ddc24
hw <hwj@marpple.com>
2023-03-10 00:42:20 +0900

    feat: add memoize function (#196)
    * feat: add memoize function
    
    * feat: support weakmap
    
    * feat: should use only Map, WeakMap

---
+++ src/memoize.ts
@@ -0,0 +1,60 @@
+/**
+ * Creates a new function that, stores the results of its calculations in a [`Map`](http://ecma-international.org/ecma-262/7.0/#sec-properties-of-the-map-prototype-object).
+ * When the function is called with same input again, it retrieves the cached result instead of recalculating it.
+ * If resolver is provided, it determines the cache key for storing the result based on the arguments provided to the memoized function.
+ * By default, the first argument provided to the memoized function is used as the map cache key
+ *
+ * @example
+ * ```ts
+ * const add10 = (a: number): number => a + 10;
+ *
+ * const memoized = memoize(add10);
+ * console.log(memoized(5)); // 15
+ * console.log(memoized(10)) // 20
+ * console.log(memoized(5)); // 15 (cached)
+ *
+ * memoized.cache.clear(); // clear cache
+ * console.log(memoized(5)); // 15 (no cache)
+ * ```
+ */
+function memoize<
+  F extends (...args: any[]) => any,
+  K extends Parameters<F>[0],
+  Return extends F & {
+    cache: K extends object ? WeakMap<K, ReturnType<F>> : Map<K, ReturnType<F>>;
+  },
+>(f: F): Return;
+function memoize<
+  F extends (...args: any[]) => any,
+  Resolver extends (...args: Parameters<F>) => any,
+  K extends ReturnType<Resolver>,
+  Return extends F & {
+    cache: K extends object ? WeakMap<K, ReturnType<F>> : Map<K, ReturnType<F>>;
+  },
+>(f: F, resolver: Resolver): Return;
+function memoize<
+  F extends (...args: any[]) => any,
+  R extends (...args: any[]) => any,
+>(f: F, resolver?: R) {
+  const memoized = (...args: Parameters<typeof f>): ReturnType<F> => {
+    const key = typeof resolver === "function" ? resolver(...args) : args[0];
+    const cache = memoized.cache;
+
+    if (!(cache instanceof WeakMap || cache instanceof Map)) {
+      throw new TypeError("`cache` should only use `WeakMap`, `Map`");
+    }
+
+    if (cache.has(key)) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return cache.get(key)!;
+    }
+    const result = f(...args);
+    cache.set(key, result);
+    return result;
+  };
+
+  memoized.cache = new Map<any, ReturnType<F>>();
+  return memoized;
+}
+
+export default memoize;
---
+++ test/memoize.spec.ts
@@ -0,0 +1,51 @@
+import { memoize } from "../src";
+
+describe("memoize", function () {
+  it("should be memoized results base on the first argument given", () => {
+    function addABC(a: number, b: number, c: number) {
+      return a + b + c;
+    }
+
+    const memoized = memoize(addABC);
+
+    expect(memoized(10, 20, 30)).toEqual(60);
+    expect(memoized(10, 20, 40)).toEqual(60);
+    expect(memoized(0, 20, 50)).toEqual(70);
+  });
+
+  it("should be supported using `resolver`", () => {
+    function addABC(a: number, b: number, c: number) {
+      return a + b + c;
+    }
+
+    const memoized = memoize(addABC, addABC);
+
+    expect(memoized(10, 20, 30)).toEqual(60);
+    expect(memoized(10, 20, 40)).toEqual(70);
+  });
+
+  it("should be supported WeakMap `cache`", () => {
+    let callCount = 0;
+    function printHelloworld(obj: { key: string }): string {
+      callCount++;
+      return obj.key + " world";
+    }
+
+    const memoized = memoize(printHelloworld);
+    memoized.cache = new WeakMap();
+
+    const obj = { key: "hello" };
+    const res1 = memoized(obj);
+    expect(res1).toEqual("hello world");
+    const res2 = memoized(obj);
+    expect(res2).toEqual("hello world");
+    expect(callCount).toEqual(1);
+  });
+
+  it("should be use only `WeakMap` or `Map` to `cache`", () => {
+    const temp = (a: number) => a;
+    const memoized = memoize(temp);
+    memoized.cache = {} as any;
+    expect(() => memoized(3)).toThrowError();
+  });
+});
