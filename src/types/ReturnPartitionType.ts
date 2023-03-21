e4b4d5723a5bb3df2e741620b1fd91d88bb832c7
sunrabbit123 <qudwls185@naver.com>
2023-03-22 02:15:44 +0900

    fix: partition types

--- src/types/ReturnPartitionType.ts
+++ src/types/ReturnPartitionType.ts
@@ -1,11 +1,8 @@
-import type Awaited from "./Awaited";
+import type { GroupBy } from "./groupBy";
 import type IterableInfer from "./IterableInfer";
 import type ReturnValueType from "./ReturnValueType";
 
 type ReturnPartitionType<T extends Iterable<unknown> | AsyncIterable<unknown>> =
-  ReturnValueType<
-    T,
-    [Awaited<IterableInfer<T>>[], Awaited<IterableInfer<T>>[]]
-  >;
+  ReturnValueType<T, [GroupBy<IterableInfer<T>, "true" | "false">]>;
 
 export default ReturnPartitionType;
