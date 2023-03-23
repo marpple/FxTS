04239aa92ec871a660a4e5a22d6bbb7255071da9
sunrabbit123 <qudwls185@naver.com>
2023-03-23 14:05:11 +0900

    fix: run prettier

--- src/types/ReturnPartitionType.ts
+++ src/types/ReturnPartitionType.ts
@@ -1,6 +1,6 @@
-import type { GroupBy } from "./groupBy";
 import type IterableInfer from "./IterableInfer";
 import type ReturnValueType from "./ReturnValueType";
+import type { GroupBy } from "./groupBy";
 
 type ReturnPartitionType<T extends Iterable<unknown> | AsyncIterable<unknown>> =
   ReturnValueType<T, [GroupBy<IterableInfer<T>, "true" | "false">]>;
