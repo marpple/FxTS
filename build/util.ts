2ab1d6db2db84beb3058472d08db0efec921eedc
dolee <panamera1594@gmail.com>
2023-05-09 14:10:42 +0900

    build: modify build script to accommodate changes in glob library API

--- build/util.ts
+++
@@ -1,9 +0,0 @@
-import glob from "glob";
-
-export const searchFiles = (pattern: string): Promise<string[]> =>
-  new Promise((resolve, reject) => {
-    glob(pattern, function (err, files) {
-      if (err) reject(err);
-      else resolve(files);
-    });
-  });
