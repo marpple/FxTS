a4a10fc7585f78858b286f1195949bbfde1d3f27
industriously <dev.rojiwon@gmail.com>
2023-06-02 17:41:37 +0900

    test: add throwIf test

--- type-check/throwIf.test.ts
+++ type-check/throwIf.test.ts
@@ -1,3 +1,5 @@
+import { randomInt } from "crypto";
+
 import { isNil, pipe, throwIf } from "../src";
 import * as Test from "../src/types/Test";
 
@@ -21,7 +23,59 @@ const res2 = pipe(
   ),
 );
 
+const res3 = throwIf(
+  (list: number[]) => list.includes(0),
+  () => new Error("0"),
+)([1, 2, 3]);
+
+const res4 = throwIf((list: number[]) => list.includes(0))([1, 2, 3]);
+
+const res5 = pipe(
+  [1, 2, 3],
+
+  throwIf((list) => list.includes(0)),
+);
+
+const res6 = pipe(
+  [1, 2, 3],
+
+  throwIf(
+    (list) => list.includes(0),
+    () => new Error("0"),
+  ),
+);
+
+// 1 | 2 | 3
+const res7 = pipe(
+  randomInt(1, 3) as 1 | 2 | 3,
+
+  throwIf((a): a is 2 => a === 2),
+);
+
+// 1 | 2 | 3
+const res8 = pipe(
+  randomInt(1, 3) as 1 | 2 | 3,
+
+  throwIf<1 | 2 | 3, 2>((a): a is 2 => a === 2),
+);
+
+// 1 | 2 | 3
+const res9 = pipe(
+  ([1, 2, 3] as const)[Math.random()],
+  throwIf(
+    (a): a is 2 => a === 2,
+    () => new Error("0"),
+  ),
+);
+
 checks([
   check<typeof res1, User, Test.Pass>(),
   check<typeof res2, string, Test.Pass>(),
+  check<typeof res3, number[], Test.Pass>(),
+  check<typeof res4, number[], Test.Pass>(),
+  check<typeof res5, number[], Test.Pass>(),
+  check<typeof res6, number[], Test.Pass>(),
+  check<typeof res7, 1 | 2 | 3, Test.Pass>(),
+  check<typeof res8, 1 | 3, Test.Pass>(),
+  check<typeof res9, 1 | 2 | 3, Test.Pass>(),
 ]);
