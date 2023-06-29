7d821f2ca1962c5efe5d1a708aefb27b3153f1c8
DoHyeong Lee <panamera1594@gmail.com>
2023-06-29 20:43:08 +0900

    fix: fix the incorrect return type of the pipe function (#217)

--- type-check/pipe.test.ts
+++ type-check/pipe.test.ts
@@ -15,6 +15,36 @@ const res6 = pipe("1", (a) => Promise.resolve(Number(a)));
 const res7 = pipe(Promise.resolve(1), (a) => Promise.resolve(String(a)));
 const res8 = pipe(Promise.resolve("1"), (a) => Promise.resolve(Number(a)));
 
+const res9 = pipe(
+  "a" as "a" | "b",
+  (a) => a.toUpperCase(),
+  () => 1,
+);
+
+const res10 = pipe(
+  Promise.resolve("a") as Promise<"a"> | "a",
+  (a) => a.toUpperCase(),
+  () => 1,
+);
+
+const res11 = pipe(
+  Promise.resolve("a") as Promise<"a"> | "a",
+  (a) => Promise.resolve(a.toUpperCase()),
+  () => 1,
+);
+
+const res12 = pipe(
+  Promise.resolve("a") as Promise<"a"> | "a",
+  (a) => a.toUpperCase(),
+  () => Promise.resolve(1),
+);
+
+const res13 = pipe(
+  Promise.resolve("a") as Promise<"a"> | "a",
+  (a) => (Math.random() > 0.5 ? a.toUpperCase() : Promise.resolve("hi")),
+  () => 1,
+);
+
 checks([
   check<typeof res1, string, Test.Pass>(),
   check<typeof res2, number, Test.Pass>(),
@@ -24,4 +54,9 @@ checks([
   check<typeof res6, Promise<number>, Test.Pass>(),
   check<typeof res7, Promise<string>, Test.Pass>(),
   check<typeof res8, Promise<number>, Test.Pass>(),
+  check<typeof res9, number, Test.Pass>(),
+  check<typeof res10, number | Promise<number>, Test.Pass>(),
+  check<typeof res11, Promise<number>, Test.Pass>(),
+  check<typeof res12, Promise<number>, Test.Pass>(),
+  check<typeof res13, number | Promise<number>, Test.Pass>(),
 ]);
