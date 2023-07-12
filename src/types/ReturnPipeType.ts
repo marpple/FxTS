ffa49ac05d828c96ae3ccc0fc39206afab8546ef
industriously <dev.rojiwon@gmail.com>
2023-07-12 14:59:09 +0900

    fix: fix ReturnPipeType

--- src/types/ReturnPipeType.ts
+++ src/types/ReturnPipeType.ts
@@ -1,7 +1,6 @@
 import type Awaited from "./Awaited";
 import type { TuplifyUnion } from "./ExcludeObject";
 import type Head from "./Head";
-import type Last from "./Last";
 import type Tail from "./Tail";
 
 type HasPromise<T extends any[]> = Head<T> extends never
@@ -12,19 +11,26 @@ type HasPromise<T extends any[]> = Head<T> extends never
   ? false
   : HasPromise<Tail<T>>;
 
-type PossiblyHasPromise<T extends any[]> = T extends []
+type PossiblyHasPromise<T extends any[]> = Head<T> extends never
   ? false
-  : HasPromise<TuplifyUnion<T[0]>> extends true
+  : HasPromise<TuplifyUnion<Head<T>>> extends true
   ? true
+  : T["length"] extends 0
+  ? false
   : PossiblyHasPromise<Tail<T>>;
 
-type ReturnPipeType<
-  T extends any[],
-  R = Awaited<Last<T>>,
-> = HasPromise<T> extends true
-  ? Promise<R>
+type ReturnPipeType<T extends any[]> = HasPromise<T> extends true
+  ? Promise<Awaited<R<T>>>
   : PossiblyHasPromise<T> extends true
-  ? Promise<R> | R
-  : R;
+  ? Promise<Awaited<R<T>>> | Awaited<R<T>>
+  : R<T>;
 
 export default ReturnPipeType;
+
+type R<T extends any[]> = T["length"] extends 0
+  ? undefined
+  : T["length"] extends 1
+  ? T[0]
+  : Awaited<T[1]> extends never
+  ? never
+  : R<Tail<T>>;
