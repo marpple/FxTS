b9f79d2e68f07f7af65b01df97b525564879134b
Vladislav Botvin <darkvlados@gmail.com>
2023-07-20 22:07:22 +0300

    feat: pipeLazy types test (#218)

--- src/Lazy/pipeLazy.ts
+++ src/Lazy/pipeLazy.ts
@@ -48,26 +48,26 @@ import type ReturnPipeType from "../types/ReturnPipeType";
 // prettier-ignore
 function pipeLazy<T1, R>(
   f1: (a: Awaited<T1>) => R
-): (a: T1) => ReturnPipeType<[T1,R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1,R]>;
 
 // prettier-ignore
 function pipeLazy<T1, T2, R>(
   f1: (a: Awaited<T1>) => T2,
   f2: (a: Awaited<T2>) => R
-): (a: T1) => ReturnPipeType<[T1, T2, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, R]>;
 
 function pipeLazy<T1, T2, T3, R>(
   f1: (a: Awaited<T1>) => T2,
   f2: (a: Awaited<T2>) => T3,
   f3: (a: Awaited<T3>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, R]>;
 
 function pipeLazy<T1, T2, T3, T4, R>(
   f1: (a: Awaited<T1>) => T2,
   f2: (a: Awaited<T2>) => T3,
   f3: (a: Awaited<T3>) => T4,
   f4: (a: Awaited<T4>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, R]>;
 
 function pipeLazy<T1, T2, T3, T4, T5, R>(
   f1: (a: Awaited<T1>) => T2,
@@ -75,7 +75,7 @@ function pipeLazy<T1, T2, T3, T4, T5, R>(
   f3: (a: Awaited<T3>) => T4,
   f4: (a: Awaited<T4>) => T5,
   f5: (a: Awaited<T5>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, R]>;
 
 function pipeLazy<T1, T2, T3, T4, T5, T6, R>(
   f1: (a: Awaited<T1>) => T2,
@@ -84,7 +84,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, R>(
   f4: (a: Awaited<T4>) => T5,
   f5: (a: Awaited<T5>) => T6,
   f6: (a: Awaited<T6>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, R]>;
 
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, R>(
   f1: (a: Awaited<T1>) => T2,
@@ -94,7 +94,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, R>(
   f5: (a: Awaited<T5>) => T6,
   f6: (a: Awaited<T6>) => T7,
   f7: (a: Awaited<T7>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, R]>;
 
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, R>(
   f1: (a: Awaited<T1>) => T2,
@@ -105,8 +105,9 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, R>(
   f6: (a: Awaited<T6>) => T7,
   f7: (a: Awaited<T7>) => T8,
   f8: (a: Awaited<T8>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, R]>;
 
+// prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(
   f1: (a: Awaited<T1>) => T2,
   f2: (a: Awaited<T2>) => T3,
@@ -117,8 +118,9 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(
   f7: (a: Awaited<T7>) => T8,
   f8: (a: Awaited<T8>) => T9,
   f9: (a: Awaited<T9>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, R]>;
 
+// prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R>(
   f1: (a: Awaited<T1>) => T2,
   f2: (a: Awaited<T2>) => T3,
@@ -130,8 +132,9 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R>(
   f8: (a: Awaited<T8>) => T9,
   f9: (a: Awaited<T9>) => T10,
   f10: (a: Awaited<T10>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R]>;
 
+// prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, R>(
   f1: (a: Awaited<T1>) => T2,
   f2: (a: Awaited<T2>) => T3,
@@ -144,8 +147,9 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, R>(
   f9: (a: Awaited<T9>) => T10,
   f10: (a: Awaited<T10>) => T11,
   f11: (a: Awaited<T11>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, R]>;
 
+// prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, R>(
   f1: (a: Awaited<T1>) => T2,
   f2: (a: Awaited<T2>) => T3,
@@ -159,8 +163,9 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, R>(
   f10: (a: Awaited<T10>) => T11,
   f11: (a: Awaited<T11>) => T12,
   f12: (a: Awaited<T12>) => R,
-): () => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, R]>;
 
+// prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, R>(
   f1: (a: Awaited<T1>) => T2,
   f2: (a: Awaited<T2>) => T3,
@@ -175,9 +180,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, R>(
   f11: (a: Awaited<T11>) => T12,
   f12: (a: Awaited<T12>) => T13,
   f13: (a: Awaited<T13>) => R,
-): () => ReturnPipeType<
-  [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, R]
->;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, R]>;
 
 // prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, R>(
@@ -195,7 +198,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, R
   f12: (a: Awaited<T12>) => T13,
   f13: (a: Awaited<T13>) => T14,
   f14: (a: Awaited<T14>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, R]>;
 
 // prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, R>(
@@ -214,7 +217,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T
   f13: (a: Awaited<T13>) => T14,
   f14: (a: Awaited<T14>) => T15,
   f15: (a: Awaited<T15>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, R]>;
 
 // prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, R>(
@@ -234,7 +237,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T
   f14: (a: Awaited<T14>) => T15,
   f15: (a: Awaited<T15>) => T16,
   f16: (a: Awaited<T16>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, R]>;
 
 // prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, R>(
@@ -255,7 +258,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T
   f15: (a: Awaited<T15>) => T16,
   f16: (a: Awaited<T16>) => T17,
   f17: (a: Awaited<T17>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, R]>;
 
 // prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, R>(
@@ -277,7 +280,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T
   f16: (a: Awaited<T16>) => T17,
   f17: (a: Awaited<T17>) => T18,
   f18: (a: Awaited<T18>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, R]>;
 
 // prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, R>(
@@ -300,7 +303,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T
   f17: (a: Awaited<T17>) => T18,
   f18: (a: Awaited<T18>) => T19,
   f19: (a: Awaited<T19>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, R]>;
 
 // prettier-ignore
 function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, R>(
@@ -324,7 +327,7 @@ function pipeLazy<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T
   f18: (a: Awaited<T18>) => T19,
   f19: (a: Awaited<T19>) => T20,
   f20: (a: Awaited<T20>) => R,
-): (a: T1) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, R]>;
+): (a: T1 | Promise<T1>) => ReturnPipeType<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, R]>;
 
 function pipeLazy(...fns: any[]) {
   return (a: any) => reduce(pipe1, a, fns);
