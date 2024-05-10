da683ce0b7c7d60aefa2eef790e10061b772a3a2
hw <xodltus@naver.com>
2024-05-10 14:02:14 +0900

    feat: expose public iterable properties

--- test/Lazy/fx.spec.ts
+++ test/Lazy/fx.spec.ts
@@ -1,7 +1,15 @@
-import { fx, toAsync } from "../../src";
+import { fx, toArray, toAsync } from "../../src";
 
 describe("fx", function () {
   describe("sync", function () {
+    it("handle fx iterable (default)", function () {
+      const res1 = [...fx([1, 2, 3, 4, 5])];
+      expect(res1).toEqual([1, 2, 3, 4, 5]);
+
+      const res2 = [...fx("abc")];
+      expect(res2).toEqual(["a", "b", "c"]);
+    });
+
     it("handle fx iterable", function () {
       const res = fx([1, 2, 3, 4, 5])
         .map((a) => a + 10)
@@ -11,6 +19,14 @@ describe("fx", function () {
   });
 
   describe("async", () => {
+    it("handle fx asyncIterable (default)", async function () {
+      const res1 = await toArray(fx(toAsync([1, 2, 3, 4, 5])));
+      expect(res1).toEqual([1, 2, 3, 4, 5]);
+
+      const res2 = await toArray(fx(toAsync("abc")));
+      expect(res2).toEqual(["a", "b", "c"]);
+    });
+
     it("handle fx asyncIterable", async function () {
       const res1 = await fx(toAsync([1, 2, 3, 4, 5]))
         .map(async (a) => a + 10)
