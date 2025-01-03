d117240846fc21cad30c931b74205c4698ba66f8
SeongPil Lee <piriri11@gmail.com>
2025-01-03 11:21:29 +0900

    chore: update text

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -13,7 +13,7 @@ function HomepageHeader() {
 
   React.useEffect(() => {
     console.log(
-      "%cHello FxTS!\n%cExperience the FxTS functions through `window.fx` in your current console.",
+      "%cHello FxTS!\n%cExperience the FxTS functions through `window.fx` in this console.",
       "color: #8f52fa; font-weight: bold; font-size: 36px;",
       "color: gray; font-size: 16px;",
     );
