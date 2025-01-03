62cdb58d5b33bd0eb76273d6b20ff21f2566ad26
SeongPil Lee <piriri11@gmail.com>
2025-01-03 11:16:01 +0900

    chore: nject fxts globally

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,5 +1,6 @@
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import * as fx from "@fxts/core";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
 import React from "react";
@@ -9,6 +10,15 @@ import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
+  React.useEffect(() => {
+    console.log(
+      "%cHello FxTS!\n%cExperience the FxTS functions through `window.fx` in your current console.",
+      "color: #8f52fa; font-weight: bold; font-size: 36px;",
+      "color: gray; font-size: 16px;",
+    );
+    window.fx = fx;
+  }, []);
 
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
