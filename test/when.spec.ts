7ce7cf801daae5f04e8b782499802abf02efa374
freevuehub <hackish113@gamil.com>
2025-03-09 13:33:18 +0900

    test: update `when` function test cases with comprehensive scenarios

--- test/when.spec.ts
+++ test/when.spec.ts
@@ -1,44 +1,33 @@
-import { isNumber, pipe, sum, when } from "../src";
+import { isNumber, isString, pipe, when } from "../src";
 
 describe("when", function () {
-  const INPUT_NUMBER_VALUE = 10;
-  const INPUT_ARRAY_VALUE = [1, 2, 3];
+  const test = <T = any>(value: T) =>
+    pipe(
+      value,
+      when(isNumber, (value) => {
+        expect(value).toBeTruthy();
+      }),
+      when(isString, () => "Hello fxts"),
+      (value) => value,
+    );
 
-  const inputNumberTest = when<string | number>(isNumber, (value) => {
-    expect(value).toBe(INPUT_NUMBER_VALUE);
-  });
-  const inputArrayTest = when<number[]>(
-    (list) => sum(list) >= 10,
-    (list) => {
-      expect(list).toEqual(expect.arrayContaining(INPUT_ARRAY_VALUE));
-    },
-  );
+  it("If the input value is a number, it will be filtered out by the first 'when' function. And throw undefined.", () => {
+    const INPUT_VALUE = 100;
 
-  it("If return of predicate is true, return is undefined.", () => {
-    expect(inputNumberTest(INPUT_NUMBER_VALUE)).toBeUndefined();
-    expect(inputArrayTest([...INPUT_ARRAY_VALUE, 4])).toBeUndefined();
+    try {
+      test(INPUT_VALUE);
+    } catch (value) {
+      expect(value).toBeUndefined();
+    }
   });
-  it("If return of predicate is false, return is same input.", () => {
-    expect(inputNumberTest("Hello World")).toBe("Hello World");
-    expect(inputArrayTest(INPUT_ARRAY_VALUE)).toBe(INPUT_ARRAY_VALUE);
-  });
-  it("Repeating the above test using pipe", () => {
-    expect(pipe(INPUT_NUMBER_VALUE, inputNumberTest)).toBeUndefined();
-    expect(pipe("Hello World", inputNumberTest)).toBe("Hello World");
-    expect(pipe([...INPUT_ARRAY_VALUE, 4], inputArrayTest)).toBeUndefined();
-    expect(pipe(INPUT_ARRAY_VALUE, inputArrayTest)).toBe(INPUT_ARRAY_VALUE);
-  });
+  it("If the input value is a string, it will be filtered out by the second 'when' function. And return value is 'Hello fxts'", () => {
+    const INPUT_VALUE = "Hello World";
 
-  // it("Repeating the above test using pipe, toAsync", async () => {
-  // await pipe(
-  //   Promise.resolve([1, 2, 3, 4]),
-  //   toAsync,
-  //   when(
-  //     (list) => sum(list) >= 10,
-  //     (list) => {
-  //       expect(list).toEqual(expect.arrayContaining(INPUT_ARRAY_VALUE));
-  //     },
-  //   ),
-  // );
-  // });
+    expect(test(INPUT_VALUE)).toBe("Hello fxts");
+  });
+  it("If nothing matches, all 'when' functions will be passed.", () => {
+    const INPUT_VALUE = [1, 2, 3, 4];
+
+    expect(test(INPUT_VALUE)).toEqual(INPUT_VALUE);
+  });
 });
