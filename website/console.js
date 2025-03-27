92e38204871e3cc19a8386e5565c11384b473361
hyunwoo jo <hwj@marpple.com>
2025-03-27 19:56:08 +0900

    feat: add global console.log (#310)

---
+++ website/console.js
@@ -0,0 +1,5 @@
+console.log(
+  "%cHello FxTS!\n%cExperience the FxTS functions through `window._` in this console.",
+  "color: #8f52fa; font-weight: bold; font-size: 36px;",
+  "color: gray; font-size: 16px;",
+);
