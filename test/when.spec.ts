60f4d6f9f057baab977e2f87bad1e97b192e2799
freevuehub <hackish113@gamil.com>
2025-03-30 12:52:23 +0900

    refactor: streamline `when` function tests by consolidating logic and enhancing pipe integration

--- test/when.spec.ts
+++ test/when.spec.ts
@@ -2,14 +2,17 @@ import { isNumber, isString, pipe, when } from "../src";
 
 describe("when", function () {
   const test = <T>(value: T) =>
+    when(
+      (value) => value === 100,
+      (value) => {
+        expect(value).toBeTruthy();
+      },
+      value,
+    );
+  const withPipe = <T>(value: T) =>
     pipe(
       value,
-      when(
-        (value) => value === 100,
-        (value) => {
-          expect(value).toBeTruthy();
-        },
-      ),
+      test,
       when(isString, () => "Hello fxts"),
     );
 
@@ -17,15 +20,18 @@ describe("when", function () {
     const INPUT_VALUE = 100;
 
     test(INPUT_VALUE);
+    withPipe(INPUT_VALUE);
   });
   it("If the input value is a string, it will be filtered out by the second 'when' function. And return value is 'Hello fxts'", () => {
     const INPUT_VALUE = "Hello World";
 
-    expect(test(INPUT_VALUE)).toBe("Hello fxts");
+    expect(test(INPUT_VALUE)).toBe("Hello World");
+    expect(withPipe(INPUT_VALUE)).toBe("Hello fxts");
   });
   it("If nothing matches, all 'when' functions will be passed.", () => {
     const INPUT_VALUE = [1, 2, 3, 4];
 
     expect(test(INPUT_VALUE)).toEqual(INPUT_VALUE);
+    expect(withPipe(INPUT_VALUE)).toEqual(INPUT_VALUE);
   });
 });
