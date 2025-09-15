288e43aa4f7f29e9f382b26042752f005889b606
Haegul Pyun <phg2491@gmail.com>
2025-09-15 22:45:02 +0900

    refactor: groupBy import variables (#350)
    * refactor: remove duplicated import variable
    
    * refactor: remove unused variables

--- src/groupBy.ts
+++ src/groupBy.ts
@@ -1,11 +1,8 @@
 import { AsyncFunctionException } from "./_internal/error";
 import { isAsyncIterable, isIterable, isPromise } from "./_internal/utils";
 import reduce from "./reduce";
-import type Cast from "./types/Cast";
 import type Equals from "./types/Equals";
-import type { GetKeyOf } from "./types/GetKeyOf";
 import type IterableInfer from "./types/IterableInfer";
-import type iterableInfer from "./types/IterableInfer";
 import type Key from "./types/Key";
 import type ReturnValueType from "./types/ReturnValueType";
 
@@ -135,7 +132,7 @@ function groupBy<
     );
   }
 
-  if (isAsyncIterable<iterableInfer<A>>(iterable)) {
+  if (isAsyncIterable<IterableInfer<A>>(iterable)) {
     return reduce(
       async (group, a) => {
         const key = await f(a);
