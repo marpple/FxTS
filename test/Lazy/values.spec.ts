953f907b1a48371ea385f0297f578eec4bb47bc0
hg-pyun <phg2491@gmail.com>
2025-12-30 15:59:02 +0900

    fix: add properties test case in keys and values

--- test/Lazy/values.spec.ts
+++ test/Lazy/values.spec.ts
@@ -15,6 +15,33 @@ describe("values", function () {
       const res = [...iter];
       expect(res).toEqual(["2", true, null, undefined]);
     });
+
+    it("should only deal with enumerable properties", function () {
+      const obj = { a: 1, b: 2, c: 3 };
+      Object.defineProperty(obj, "a", {
+        enumerable: false,
+      });
+      Object.defineProperty(obj, "c", {
+        enumerable: false,
+      });
+      const res = [...values(obj)];
+      expect(res).toEqual([2]);
+    });
+
+    it("should only deal with its own properties", function () {
+      class Parent {
+        a = 1;
+      }
+      class Child extends Parent {
+        b = 2;
+      }
+      const instance = new Child();
+      const proto = Object.getPrototypeOf(instance);
+      proto.__proto__.c = 3;
+      const res = [...values(instance)];
+      expect((instance as any).c).toEqual(3);
+      expect(res).toEqual([1, 2]);
+    });
   });
 
   describe("Map support", function () {
