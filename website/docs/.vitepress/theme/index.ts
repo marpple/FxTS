d380f7c9c39dd58819057adf051a77ba4586d2fd
hg-pyun <phg2491@gmail.com>
2026-03-25 23:11:28 +0900

    feat: add translation banner for untranslated pages
    Show a warning banner at the top of doc pages when `translated: false`
    is set in frontmatter. The banner displays a localized message and a
    link to contribute translations.

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -1,5 +1,11 @@
+import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+
+import Layout from "./Layout.vue";
 
 import "./style.css";
 
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+  Layout,
+} satisfies Theme;
